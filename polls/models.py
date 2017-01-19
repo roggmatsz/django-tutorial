07905b690a180a9866cae3c52c413475372fad09
Rogger Matamoros <roggmatz@gmail.com>
2017-01-19 11:59:59 -0500

    Added custom function, supporting imports.

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -6,9 +8,11 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
