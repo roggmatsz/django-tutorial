53f4c89a6d3d1f411bf855419fcffa5c4c189fba
Rogger Matamoros <roggmatz@gmail.com>
2017-01-20 21:04:23 -0500

    Refactored vote view to handle submitted data.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from .models import Question, Choice
 from django.template import loader
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -20,6 +21,16 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-    
-# Create your views here.
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, \
+                      'polls/detail.html', \
+                      {'question': question, 'error_message': "No choice set."})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        #Always redirect after dealing with POST data
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
